6def4bbc63d3f514909abae274a800a7aca253fa
李登淳 <mikecovlee@163.com>
2023-04-07 16:19:32 +0800

    add csbuild

---
+++ csbuild/make.bat
@@ -0,0 +1,3 @@
+@echo off
+cs misc\releasegen.csc
+cspkg build --release https://raw.githubusercontent.com/mikecovlee/parsergen/main/cspkg-repo/
---
+++ csbuild/make.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cs misc/releasegen.csc
+cspkg build --release https://raw.githubusercontent.com/mikecovlee/parsergen/main/cspkg-repo/
